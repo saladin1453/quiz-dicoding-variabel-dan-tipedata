df2a303cae208a2c54ab28e03241862784bf6f36
saladin1453 <nazirazhari04@gmail.com>
2021-04-12 21:42:22 +0700

    membuat variabel dan menginisiasikan berdasarkan referensi dari soal

---
+++ app.js
@@ -0,0 +1,15 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+// TODO
+const firstName = 'Muhammad';
+const lastName = 'Nazir Azhari';
+const age = 19;
+const isMarried = false;
+/**/
